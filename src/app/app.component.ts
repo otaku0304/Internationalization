24743a3e538ad16372494decb43483ee61d39814
otaku0304 <107242846+otaku0304@users.noreply.github.com>
2023-09-03 13:26:23 +0530

    project files added succesfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'internationalization';
+  currentYear: number = new Date().getFullYear();
+
+  siteLanguage: any;
+  siteLocale = 'en';
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'nl', label: 'Netherlands' },
+    { code: 'sr', label: 'Serbian' },
+  ];
+  ngOnInit() {
+    this.siteLocale = window.location.pathname.split('/')[1];
+    this.siteLanguage = this.languageList.find(
+      (f) => f.code === this.siteLocale
+    )?.label;
+    this.currentYear = new Date().getFullYear();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  HashLocationStrategy,
+  Location,
+  LocationStrategy,
+} from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, routingComponents],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
