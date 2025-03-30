5e3ca64c5d7ec2428700a046530d33379d49c1ff
otaku0304 <107242846+otaku0304@users.noreply.github.com>
2025-03-30 16:53:14 +0530

    feat(components): update components to be non-standalone and improve dependency management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false
 })
 export class AppComponent {
   currentYear: number = new Date().getFullYear();
