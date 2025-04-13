6b3d727d24547b678b9f72055507a219a91f1d14
Sai Annam <107242846+otaku0304@users.noreply.github.com>
2025-04-13 10:27:41 +0530

    feat: update Firebase deployment workflow for Angular localization support

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import {
-  HashLocationStrategy,
-  Location,
-  LocationStrategy,
-} from '@angular/common';
 import '@angular/localize/init';
 
 bootstrapApplication(AppComponent, {
@@ -19,6 +14,5 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideRouter(routes),
-    Location, { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 }).catch(err => console.error(err));
