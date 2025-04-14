d88ac173e8cacfff278e57938c91056c1a7c44ec
Sai Annam <107242846+otaku0304@users.noreply.github.com>
2025-04-14 12:11:34 +0530

    feat: enhance SEO by updating meta tags and title based on route data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './i18n/navbar/navbar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { Meta, Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -29,11 +37,44 @@ export class AppComponent {
     { code: 'nl', label: 'Netherlands' },
     { code: 'sr', label: 'Serbian' },
   ];
+
+  private readonly siteUrl = "https://internationalization-d2aa6.web.app/en/";
+
+  constructor(
+    private readonly meta: Meta,
+    private readonly title: Title,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) { }
+
   ngOnInit() {
     this.siteLocale = window.location.pathname.split('/')[1];
     this.siteLanguage = this.languageList.find(
       (f) => f.code === this.siteLocale
     )?.label;
     this.currentYear = new Date().getFullYear();
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let child = this.route.firstChild;
+          while (child?.firstChild) {
+            child = child.firstChild;
+          }
+          return child?.snapshot.data;
+        })
+      )
+      .subscribe((routeData) => {
+        const currentUrl = `${this.siteUrl}${this.router.url}`;
+        if (routeData) {
+          this.title.setTitle(routeData['title'] || 'Default Title');
+          this.meta.updateTag({
+            name: 'description',
+            content: routeData['description'] || 'Default Description',
+          });
+          this.meta.updateTag({ name: 'canonical', content: currentUrl });
+          this.meta.updateTag({ property: 'og:url', content: currentUrl });
+        }
+      });
   }
 }
